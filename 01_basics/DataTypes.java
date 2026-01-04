0c36f5a62594c446a9d789debef8059efb4203fe
Amey Deshmukh <ameydeshmukh.work@gmail.com>
2026-01-04 18:10:18 +0530

    Data Types

---
+++ 01_basics/DataTypes.java
@@ -0,0 +1,15 @@
+public class DataTypes {
+    public static void main(String[] args) {
+     byte b= 8;
+     System.out.println(b);   
+     char ch = 'a'; 
+     System.out.println(ch);
+     boolean var = true;
+     float price = 10.5f;
+     int number = 25;
+    // long    use when int range is not enough
+// double  use for higher precision decimal values
+// short   rarely used
+
+    }
+}
