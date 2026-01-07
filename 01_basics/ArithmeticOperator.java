678f5213a589e6b1c712cd4221601f5e1f97a668
Amey Deshmukh <ameydeshmukh.work@gmail.com>
2026-01-07 20:30:44 +0530

    Arithmetic Operator

---
+++ 01_basics/ArithmeticOperator.java
@@ -0,0 +1,20 @@
+public class ArithmeticOperator {
+    public static void main(String[] args) {
+     
+    int a = 10;
+    int b = 5;
+
+    System.out.println("a + b = " + (a + b)); // Addition
+
+    System.out.println("a - b = " + (a - b)); // Subtraction
+
+    System.out.println("a * b = " + (a * b)); // Multiplication
+
+    System.out.println("a / b = " + (a / b)); // Division
+
+    System.out.println("a % b = " + (a % b)); // Modulus
+
+
+        
+    }
+}
