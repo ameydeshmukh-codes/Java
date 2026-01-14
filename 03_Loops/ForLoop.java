119294d0218068ee12cfd0efddc2e1d95c71ef2c
Amey Deshmukh <ameydeshmukh.work@gmail.com>
2026-01-14 22:09:29 +0530

    For Loop

---
+++ 03_Loops/ForLoop.java
@@ -0,0 +1,23 @@
+
+
+public class ForLoop {
+    public static void main(String[] args) {
+       
+        // Example of a for loop that prints numbers from 1 to 5
+        
+        System.out.println("Incrementing loop:");
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i);
+        }
+
+        System.out.println("Decrementing loop:");
+        for (int i = 5; i >= 1; i--) {
+            System.out.println(i);
+
+
+        }
+    }
+        }
+
+    
+
